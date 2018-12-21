99d10c86193eaa088041675247e4bfa7c235a45b
Landon Gravat <304961@sps.org>
2018-12-21 23:35:33 +0000

    add unfuck command

---
+++ src/commands/unfuck.js
@@ -0,0 +1,18 @@
+const Unfuck = require('../interpreters/unfuck');
+const { formatOutput, stripNewlines } = require('../utils');
+
+exports.run = (bot, msg, args) => {
+    args = args.join(' ').split('|');
+    const source = stripNewlines(args[0].trim());
+    const input = args[1] ? args[1] : '';
+    const output = new Unfuck(source, input).exec();
+    msg.channel.send({
+        embed: formatOutput('Unfuck', source, input, output, 0x2F2530)
+    });
+};
+
+exports.help = {
+    name: 'brainfuck',
+    usage: '<code>[ | <input>]',
+    aliases: ['bf', 'brainfk']
+};
